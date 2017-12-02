0bec873dfc5d116e83954547a76b4813114f851d
Christopher F <foxbot@protonmail.com>
2017-12-01 20:06:48 -0500

    Added part two to day one

--- 2017/1_captcha.c
+++ 2017/1_captcha.c
@@ -5,15 +5,26 @@
 
 void fail(const char* reason, ...);
 int* parseInput(const char* chars, const int len);
+int* parsePart(const char* chars);
 
 void main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fail("USAGE: captcha <captcha>\n");
+    if (argc != 3) {
+        fail("USAGE: captcha <captcha> <part>\n");
     }
 
     char* chars = argv[1];
     int len = strlen(chars);
+    if (len % 2 != 0) {
+        fail("ERROR: input string must contain an even quantity of numbers");
+    }
     int* nums = parseInput(chars, len);
+    int* part = parsePart(argv[2]);
+
+    int distance;
+    if (*part == 1) distance = 1;
+    else {
+        distance = len / 2;
+    }
 
     int counter = 0;
 
@@ -21,7 +32,9 @@ void main(int argc, char *argv[]) {
     for (i = 0; i < len; i++) {
         int cur = nums[i];
         // nums is a circular list
-        int next = i != len-1 ? nums[i+1] : nums[0];
+        int nextPos = i+distance;
+        if (nextPos >= len) nextPos-=len;
+        int next = nums[nextPos];
 
         if (cur == next) {
             counter += cur;
@@ -45,6 +58,15 @@ int* parseInput(const char* chars, const int len) {
     }
 
     return nums;
+}
+int* parsePart(const char* chars) {
+    int* number = malloc(sizeof(int));
+    int len = strlen(chars);
+    if (len != 1 || (chars[0] < '1' || chars[0] > '2')) {
+        fail("ERROR: part must be one of '1' or '2'");
+    }
+    *number = chars[0] - '0';
+    return number;
 }
 
 void fail(const char* reason, ...) {
--- 2017/1_captcha.md
+++ 2017/1_captcha.md
@@ -6,15 +6,26 @@ A C compiler (I use GCC)
 
 ### Building
 
-`$ gcc ./1_captcha.c -o 1_captcha`
+`$ gcc ./1_captcha.c -o 1_captcha.o`
 
 ### Usage
 
-`$ ./1_captcha $(cat ./1_captcha-in.txt)`
+#### Part 1
+
+`$ ./1_captcha.o $(cat 1_captcha-in.txt) 1`
+
+#### Part 2
+
+`$ ./1_captcha.o $(cat 1_captcha-in.txt) 2`
 
 ### Sample
 
 ```
-$ ./1_captcha $(cat 1_captcha-in.txt)
+$ ./1_captcha.o $(cat 1_captcha-in.txt) 1
 1034
+```
+
+```
+$ ./1_captcha.o $(cat 1_captcha-in.txt) 2
+1356
 ```
